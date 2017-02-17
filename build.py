07409257207903263a3d78482881b19ae9f48154
Mathias Stearn <redbeard0531@gmail.com>
2017-02-17 11:25:47 -0500

    msvc_linker fix - try 2

--- build.py
+++ build.py
@@ -19,6 +19,7 @@ except ImportError:
     sys.path.append(my_dir)
     import ninja_syntax
 
+split_lines_script = os.path.join(my_dir, 'split_lines.py')
 subst_file_script = os.path.join(my_dir, 'subst_file.py')
 test_list_script = os.path.join(my_dir, 'test_list.py')
 
@@ -306,16 +307,10 @@ class NinjaFile(object):
         libdeps = []
         if tool in ('LINK', 'SHLINK'):
             libdeps = myEnv.subst('$_LIBDEPS',executor=n.executor)
-            if self.globalEnv.ToolchainIs('msvc'):
-                # On windows we need to use $in_newline so the libdeps need to go in inputs.
-                # This works around the fixed-size 128KB buffer used to read each line...
-                cmd.replace('$in', '$in_newline')
-                sources = libdeps.split() + sources
-                libdeps = []
-                n.executor.get_lvars()['_LIBDEPS'] = '' # prevent expansion
-            else:
-                n.executor.get_lvars()['_LIBDEPS'] = libdeps # cache the result.
-                libdeps = libdeps.split()
+            n.executor.get_lvars()['_LIBDEPS'] = libdeps # cache the result.
+            libdeps = libdeps.split()
+            if myEnv.ToolchainIs('msvc'):
+                implicit_deps += split_lines_script
 
         myVars = {}
 
@@ -331,7 +326,7 @@ class NinjaFile(object):
 
             mySubst = myEnv.subst(word, executor=n.executor)
 
-            if name in ('_LIBFLAGS', '_PDB', '_MSVC_OUTPUT_FLAG') and mySubst:
+            if name in ('_LIBFLAGS', '_PDB', '_MSVC_OUTPUT_FLAG'):
                 # These are never worth commoning since they are always different.
                 myVars[name] = mySubst
                 continue
@@ -478,7 +473,7 @@ class NinjaFile(object):
                     description = 'CC $out')
             if 'LINK' in self.tool_commands:
                 ninja.rule('LINK',
-                    command = '$LINK @$out.rsp',
+                    command = 'cmd /c $PYTHON %s $out.rsp && $LINK @$out.rsp'%split_lines_script,
                     rspfile = '$out.rsp',
                     rspfile_content = self.tool_commands['LINK'].replace('$LINK ', ''),
                     description = 'LINK $out')
